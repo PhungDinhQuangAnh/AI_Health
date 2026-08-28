3ec87f5d718b6ba1fc31a519e92a398f4fdddaa6
Phùng Đình Quang Anh <pd.quanganh.work@gmail.com>
2026-08-28 15:57:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from Loan_Default_Prediction.loan_default_app import run_loan_default_app
 from Credit_Card_Segmentation.credit_card_app import run_credit_card_app
 
 # --- Cấu hình trang ---
-st.set_page_config(page_title="Banking Intelligence Platform", page_icon=":bank:", layout="wide", initial_sidebar_state=320)
+st.set_page_config(page_title="Banking Intelligence Platform", page_icon=":bank:", layout="wide", initial_sidebar_state=345)
 
 # --- Nạp thư viện Icon ---
 st.markdown('<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">', unsafe_allow_html=True)
@@ -14,7 +14,7 @@ st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs
 st.markdown("""
     <style>
         .sidebar-header {
-            font-size: 23px;
+            font-size: 25px;
             font-weight: 800;
             color: #1B4F72;
             border-bottom: 3px solid #2980B9;
